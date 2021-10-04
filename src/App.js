a20056a2773b466af1cf95c917215828379af06d
Elio Lu <1520622848@qq.com>
2021-10-04 10:27:43 +0800

    :octocat: pages and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home'
+import About from './pages/About'
+import Articles from './pages/Articles'
+import Article from './pages/Article'
+import NavBar from './NavBar'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} exact />
+          <Route path="/article-list" component={Articles} exact />
+          <Route path="/article/:name" component={Article} exact />
+        </div>
+      </div>
+    </Router>
   );
 }
 
